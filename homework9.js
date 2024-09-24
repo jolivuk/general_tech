f8582d8211dd513b2e440afd799288c08d4834aa
Nina Janeckova <160600083+jolivuk@users.noreply.github.com>
2024-09-24 09:57:20 +0200

    homework 9 is added

---
+++ homework9.js
@@ -0,0 +1,28 @@
+use homeworks
+
+db.users.insertMany([{
+    _id: 1,
+    first_name: 'Анатолий', 
+    last_name: 'Ушанов', 
+    age: '28',
+    gender: 'm'
+},{
+    _id: 2,
+    first_name: 'Светлана', 
+    last_name: 'Демидова', 
+    age: '25',
+    gender: 'f'
+},{
+    _id: 3,
+    first_name: 'Никита', 
+    last_name: 'Иванов', 
+    age: '33',
+    gender: 'm'
+},{
+    _id: 4,
+    first_name: 'Ольга', 
+    last_name: 'Петрова', 
+    age: '22',
+    gender: 'f'
+}])
+
